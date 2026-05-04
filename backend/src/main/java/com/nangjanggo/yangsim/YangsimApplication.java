40f8380d887d4560666bfb4d77b8cb7e2f9caeb4
dodo <cheeh0103@ajou.ac.kr>
2026-05-04 19:17:11 +0900

    통합

---
+++ backend/src/main/java/com/nangjanggo/yangsim/YangsimApplication.java
@@ -0,0 +1,12 @@
+package com.nangjanggo.yangsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YangsimApplication.class, args);
+	}
+}
