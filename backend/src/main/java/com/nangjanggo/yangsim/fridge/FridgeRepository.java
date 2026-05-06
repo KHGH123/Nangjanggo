13657e3a8b374bc367d5fde2b9a349e3384b5412
KihunLee <kihoon9074@ajou.ac.kr>
2026-05-06 22:15:08 +0900

    통합

--- backend/src/main/java/com/nangjanggo/yangsim/fridge/FridgeRepository.java
+++ backend/src/main/java/com/nangjanggo/yangsim/fridge/FridgeRepository.java
@@ -2,6 +2,7 @@ package com.nangjanggo.yangsim.fridge;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface FridgeRepository extends JpaRepository<Fridge, Long> {
 
