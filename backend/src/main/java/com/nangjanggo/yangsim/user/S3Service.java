742463342457165564aa059dfefde639f4951620
KihunLee <kihoon9074@ajou.ac.kr>
2026-05-20 18:44:12 +0900

    찐막

---
+++ backend/src/main/java/com/nangjanggo/yangsim/user/S3Service.java
@@ -0,0 +1,55 @@
+package com.nangjanggo.yangsim.user;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+    private final String bucket;
+
+    public S3Service(
+            @Value("${aws.credentials.access-key}") String accessKey,
+            @Value("${aws.credentials.secret-key}") String secretKey,
+            @Value("${aws.s3.region}") String region,
+            @Value("${aws.s3.bucket}") String bucket) {
+        this.bucket = bucket;
+        this.s3Client = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+
+    public String upload(MultipartFile file) throws IOException {
+        String key = "profile/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+        s3Client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(key)
+                        .contentType(file.getContentType())
+                        .build(),
+                RequestBody.fromBytes(file.getBytes()));
+        return "https://" + bucket + ".s3." + s3Client.serviceClientConfiguration().region().id() + ".amazonaws.com/" + key;
+    }
+
+    public void delete(String imageUrl) {
+        String key = imageUrl.substring(imageUrl.indexOf("profile/"));
+        s3Client.deleteObject(DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build());
+    }
+}
